b320f84e7282b90f162d8ef89f046eb5941971bd
manucastelnovo <marnutskate@gmail.com>
2024-12-28 12:46:41 -0300

    counter component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@
 // import { BasicFunctions } from "./typescript/BasicFunctions";
 // import { ObjectsLiterals } from "./typescript/ObjectsLiterals";
 
+import { Counter } from "./components/Counter";
+
 function App() {
   return (
     <>
-      <h1>react + typescript</h1>
-      {/* <BasicTypes /> */}
-      {/* <ObjectsLiterals /> */}
-      {/* <BasicFunctions /> */}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex flex-col justify-center items-center h-svh">
+        <h1>react + typescript</h1>
+        {/* <BasicTypes /> */}
+        {/* <ObjectsLiterals /> */}
+        {/* <BasicFunctions /> */}
+        <h1 className="text-4xl mb-5">Hello world!</h1>
+        <Counter />
+      </div>
     </>
   );
 }
